cae16913bc12b31da643063f7b89492cfb911d24
Lars De Maere <demaela@cronos.be>
2022-05-17 14:41:14 +0200

    Changes to Automated request to reflect assignment

--- cypress/integration/apiCalls.spec.js
+++ cypress/integration/apiCalls.spec.js
@@ -1,49 +1,16 @@
+/// <reference types="cypress" />
+
 describe('Pet store API Intercept test', () => {
     it('Intercepting API response', () => {
-        
         
         cy.intercept('POST','https://petstore3.swagger.io/api/v3/pet',(req) => {
             req.body.name = "Intercepted"
         } ).as('petPost');
 
         cy.visit('https://petstore3.swagger.io')
-        cy.contains('POST').click()
+        cy.contains('Add a new pet to the store').click()
         cy.contains('Try it out').click()
-        cy.fixture('petStorePost.json').then((petFixture) => {
-            cy.get('textarea').click().clear().type(`{
-                "id": 155,
-                "name": "not intercepted",
-                "category": {
-                  "id": 155,
-                  "name": "not intecepted"
-                },
-                "photoUrls": [
-                  "string"
-                ],
-                "tags": [
-                  {
-                    "id": 0,
-                    "name": "string"
-                  }
-                ],
-                "status": "available"
-              }`)
-            
-        })
         cy.contains('Execute').click()
-
-
-        /*
-        cy.fixture('petStorePost.json').then((petFixture) => {
-            cy.get('textarea').click().clear().type(petFixture)
-            //cy.request('POST', 'https://petstore3.swagger.io/api/v3/pet',petFixture);
-        })*/
-
-        /*
-        cy.wait(4000)
-        cy.fixture('petStorePost.json').then((petFixture) => {
-            cy.request('POST', 'https://petstore3.swagger.io/api/v3/pet',petFixture);
-        })*/
 
         cy.wait('@petPost').then (req => {
             console.log(req)
--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -3,13 +3,9 @@ describe('Pet store API Intercept test', () => {
         
         
         cy.intercept('GET','https://petstore3.swagger.io/api/v3/pet/*',(req) => {
-            req.body.id = 15
+            req.url = 'https://petstore3.swagger.io/api/v3/pet/15'
         } ).as('petPost');
-
-        cy.fixture('petStorePost.json').then((petFixture) => {
-            cy.request('GET', 'https://petstore3.swagger.io/api/v3/pet/1');
-        })
-
+        cy.request('GET', 'https://petstore3.swagger.io/api/v3/pet/1');
         cy.wait('@petPost').then (req => {
             console.log(req)
             //expect(req.request.body.name).to.equal("Intercepted")
